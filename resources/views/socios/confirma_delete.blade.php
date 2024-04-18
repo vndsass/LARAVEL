dc63ca8ae7d54186724cccdd567fc8c63c1930fa
vndsass <159710111+vndsass@users.noreply.github.com>
2024-04-18 15:04:24 +0100

    alteracao na funcionalidade de elimincao
    foi realocado o botao de eliminacao do socio

---
+++ resources/views/socios/confirma_delete.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Confirmar Eliminação do Sócio') }}</div>
+
+                <div class="card-body">
+                    <p>Confirma a eliminição deste sócio?</p>
+                    <p><strong>Atenção: O sócio será eliminado definitivamente</strong></p>
+                    <form action="{{route('socio.destroy', $id->id)}}" method="post">
+                    @csrf
+                    @method('DELETE')    
+                    <button class="btn btn-secondary-danger" type="delete" value="limpar">Eliminar definitivamente</button>                               
+                </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
